d03eab1878a61742899dd7b09d1827090b00eaa0
Kevin <mercury0110111@gmail.com>
2025-06-18 00:51:30 -0700

    added git plugin

--- movementPlayer.gd
+++ movementPlayer.gd
@@ -2,23 +2,26 @@ extends CharacterBody2D
 
 @export var speed = 200
 @export var rotation_speed = 1.1
+#@export var controller: Script
 
 var rotation_direction = 0
+var movement_direction = 0
 
 func get_input():
 	rotation_direction = Input.get_axis("dir_Left", "dir_Right")
-	velocity += transform.x * Input.get_axis("dir_Down", "dir_Up") * 10
+	movement_direction = Input.get_axis("dir_Down", "dir_Up")
+	velocity += transform.x * movement_direction * 10
 	if velocity.length() > speed:
 		velocity = speed * velocity.normalized()
 
 func _physics_process(delta):
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
-	if velocity.length() > 4:
-		velocity *= (1 - 0.5 * delta)
-	else:
-		velocity = Vector2.ZERO
-		
-	print(velocity.length())
+	
+	if rotation_direction == 0 and movement_direction == 0:
+		if velocity.length() < 5:
+			velocity = Vector2.ZERO
+		else:
+			velocity = min(1, delta/0.018) * velocity
 		
 	move_and_slide()
